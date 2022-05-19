98730f03aae41955d01bae44292af4bf1cd6f6b9
Scarlate <scarlate100@gmail.com>
2022-05-18 23:41:20 -0300

    feat: add card

---
+++ src/components/card/card-link.tsx
@@ -0,0 +1,45 @@
+import  Image  from 'next/image'
+import { useRouter } from 'next/router'
+import { Container, Item, Imagem, ContainerImage, CartStyles } from "./styles"
+import add from '../../assets/images/add.png'
+import inferior from '../../assets/images/inferior.png'
+import play from '../../assets/images/play-button.png'
+import { Flex, Heading } from 'rebass'
+
+
+interface CardProps {
+  src?: any
+  title?: string
+  id?: any
+}
+export const CardLink = ({ src, title, id }: CardProps) => {
+  const router = useRouter()
+  
+  return(
+    <>
+    <Container>
+      <Item>
+        <Imagem src={src}/>
+        <ContainerImage>
+          <Heading fontSize='1rem' my='5px'>{title}</Heading>
+        <Flex mt='10px' justifyContent='space-between' style={{ gap: '0.7rem'}}>
+          <Flex style={{ gap: '1rem'}}>
+            <CartStyles>
+            <Image src={play} width='30px' height='30px' alt='img'  />
+            </CartStyles>
+            <CartStyles>
+            <Image src={add} width='30px' height='30px' alt='img'  />
+            </CartStyles>
+          </Flex>
+          <Flex>
+            <CartStyles onClick={() => router.push(`/move/${id}`)}>
+            <Image src={inferior} width='30px' height='30px' alt='img'  />
+            </CartStyles>
+          </Flex>
+       </Flex>
+        </ContainerImage>
+      </Item>
+    </Container>
+    </>
+  )
+}
