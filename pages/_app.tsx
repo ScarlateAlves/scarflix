c44d66046d935fec8730ab092d03d5bb6ace03a4
Scarlate <scarlate100@gmail.com>
2022-05-18 23:39:11 -0300

    feat: add query request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NavBar } from '../src/components/nav-bar';
+import { Layout } from '../src/components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+     <Layout>
+     <Component {...pageProps} />
+     </Layout>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
