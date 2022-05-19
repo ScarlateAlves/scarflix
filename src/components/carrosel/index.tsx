339b86576afe12fee2dfcdb60c96c91c87a7b07e
Scarlate <scarlate100@gmail.com>
2022-05-18 23:41:33 -0300

    feat: add carrosel

---
+++ src/components/carrosel/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Heading } from 'rebass'
+import { CardLink } from '../card/card-link'
+import { MultipleItems } from '../scroll/scroll-lateral'
+import { image } from '../../utils/index'
+
+interface CarroselProps {
+  title?: string
+  name?: string
+  list?: any | []
+}
+
+export const Carrosel = ({ title, list }: CarroselProps) => {
+ return(
+  <>
+  <Heading>{title}</Heading>
+   {list ?
+    <MultipleItems>
+    { list.map((item: any) => (
+        <CardLink id={item.id} key={item.id} src={`${image}${item.poster_path}`} title={item.title ? item.title : item.name}/>
+       ))}
+    </MultipleItems>
+    :
+    ''
+   }
+ </>
+ )
+}
