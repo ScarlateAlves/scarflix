83e98087558f9e1674148aed579dc6e95a308b27
Scarlate <scarlate100@gmail.com>
2022-05-27 22:45:13 -0300

    add pages dinamic

--- pages/genre/[genreId].tsx
+++ pages/genre/[genreId].tsx
@@ -1,11 +1,26 @@
 import { NextPage } from "next"
-import { Heading } from "rebass"
+import { useRouter } from "next/router"
+import { Flex, Heading } from "rebass"
+import { CardLink } from "../../src/components/card/card-link"
+import { useMovieDiscover } from "../../src/hooks/genres/discover-movie"
+import { image } from "../../src/utils"
 
 const Genre: NextPage  = () => {
+  const router = useRouter()
+  const { genreId } = router.query
+  const { data } = useMovieDiscover(String(genreId))
+  console.log(data)
 
   return(
     <>
-    <Heading mt='70px' color='red'>Genero</Heading>
+    <Heading fontSize='2rem' fontFamily='Montserrat, sans-serif' mt='70px'>Genero</Heading>
+    <Flex flexWrap='wrap' justifyContent='center'>
+    {data?.results.map((item: any) => (
+          <Flex m='5px' key={item.id}>
+             <CardLink id={item.id} src={`${image}${item.poster_path}`} title={item.title ? item.title : item.name} />
+          </Flex>
+        ))}
+    </Flex>
     </>
   )
 }
